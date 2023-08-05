dc53d36b2c35335b002d69913ef2e87a55b2f97a
marcelomoresco <marcelomoresco0@gmail.com>
2023-08-05 00:05:36 -0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Marcelo Moresco on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
